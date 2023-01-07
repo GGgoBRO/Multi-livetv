26194650656a87f5dbdae316bb1ff9f6a6b5962b
GGgoBRO <122142328+GGgoBRO@users.noreply.github.com>
2023-01-07 18:12:27 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ subprojects {
     cloudstream {
         // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
         // you can modify it to use other git hosting services, like gitlab
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/daarkdemon/cs-darkdemon-extensions")
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/GGgoBRO/Multi-livetv")
     }
 
     android {
